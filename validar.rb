fb5198fa0aa773bf8fcc5ac8fd7a64b1f833627c
Anibal Linares <anibal.linares.19@gmail.com>
2019-04-17 18:53:02 -0400

    Se agrega comentarios

--- validar.rb
+++ validar.rb
@@ -13,10 +13,13 @@ end
 
 puts "La edad es #{edad1}"
 puts validar_edad(edad1)
+# Se agrega puts  para ver que edad esta generando
 
 puts "La edad es #{edad2}"
 puts validar_edad(edad2)
+# Se agrega puts  para ver que edad esta generando
 
 
 puts "La edad es #{edad3}"
 puts validar_edad(edad3)
+# Se agrega puts  para ver que edad esta generando 
